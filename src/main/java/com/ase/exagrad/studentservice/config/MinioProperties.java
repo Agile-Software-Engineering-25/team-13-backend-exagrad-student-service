a342f0742bdd1c57b57394036084ccaeaa89cd8b
py-bay <github.com.tribunal325@passmail.net>
2025-09-02 12:03:24 +0200

    add MinioProperties.java

---
+++ src/main/java/com/ase/exagrad/studentservice/config/MinioProperties.java
@@ -0,0 +1,18 @@
+package com.ase.exagrad.studentservice.config;
+
+import lombok.Data;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Data
+@Component
+@ConfigurationProperties(prefix = "minio")
+public class MinioProperties {
+    private String url;
+    private String accessKey;
+    private String secretKey;
+    private Map<String, String> buckets;
+}
