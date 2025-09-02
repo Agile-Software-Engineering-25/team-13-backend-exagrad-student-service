ce1ec638c42cef5e19cfaee34ee62a77f14358ec
py-bay <github.com.tribunal325@passmail.net>
2025-09-02 17:07:48 +0200

    simplify exam document request / response Modals

--- src/main/java/com/ase/exagrad/studentservice/dto/ExamDocumentRequest.java
+++ src/main/java/com/ase/exagrad/studentservice/dto/ExamDocumentRequest.java
@@ -4,7 +4,6 @@ import lombok.Data;
 
 @Data
 public class ExamDocumentRequest {
-  private String title;
-  private String examId;
-  private String studentId;
+    private String examId;
+    private String studentId;
 }
---
+++ src/main/java/com/ase/exagrad/studentservice/dto/ExamDocumentResponse.java
@@ -0,0 +1,16 @@
+package com.ase.exagrad.studentservice.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.Instant;
+
+@Data
+@Builder
+public class ExamDocumentResponse {
+    private String examId;
+    private String studentId;
+    private Instant uploadDate;
+    private String minioKey;
+    private String fileName;
+}
