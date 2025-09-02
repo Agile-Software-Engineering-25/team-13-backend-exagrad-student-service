b4bc8cd390ae243ed730379859fce51158e640e3
py-bay <github.com.tribunal325@passmail.net>
2025-09-02 17:10:26 +0200

    remove title from ExamDocument entity and reformat file

--- src/main/java/com/ase/exagrad/studentservice/entities/ExamDocument.java
+++ src/main/java/com/ase/exagrad/studentservice/entities/ExamDocument.java
@@ -5,14 +5,17 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import java.time.Instant;
-import java.util.UUID;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
 import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+import java.util.UUID;
 
 @Entity
 @Table(name = "exam_documents")
@@ -23,30 +26,18 @@ import org.hibernate.annotations.CreationTimestamp;
 @Builder
 public class ExamDocument {
 
-  @Id
-  @GeneratedValue
-  private UUID id;
+    @Id @GeneratedValue private UUID id;
 
-  @Column(nullable = false)
-  private String title;
+    @Column(nullable = false)
+    private String examId;
 
-  /**
-   * Refers to the exam this document belongs to.
-   */
-  @Column(nullable = false)
-  private String examId;
+    @Column(nullable = false)
+    private String studentId;
 
-  /**
-   * Refers to the student/user this document belongs to.
-   * TODO: maybe we have to distinguish later between 'user belongs to' and 'user uploaded'
-   */
-  @Column(nullable = false)
-  private String studentId;
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private Instant uploadDate;
 
-  @CreationTimestamp
-  @Column(nullable = false, updatable = false)
-  private Instant uploadDate;
-
-  @Column(nullable = false, unique = true)
-  private String minioKey;
+    @Column(nullable = false, unique = true)
+    private String minioKey;
 }
