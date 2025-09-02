aba59df4d0785f997ea1e20da362a4c6220a1182
py-bay <github.com.tribunal325@passmail.net>
2025-09-02 17:08:19 +0200

    add methods to optimized query ExamDocumentRepository

--- src/main/java/com/ase/exagrad/studentservice/repositories/ExamDocumentRepository.java
+++ src/main/java/com/ase/exagrad/studentservice/repositories/ExamDocumentRepository.java
@@ -1,8 +1,14 @@
 package com.ase.exagrad.studentservice.repositories;
 
 import com.ase.exagrad.studentservice.entities.ExamDocument;
-import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.UUID;
+
 public interface ExamDocumentRepository extends JpaRepository<ExamDocument, UUID> {
+    List<ExamDocument> findByStudentId(String studentId);
+
+    List<ExamDocument> findByExamId(String examID);
 }
