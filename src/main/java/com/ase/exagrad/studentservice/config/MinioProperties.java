5c6546ded731432e1d73be2f2079b2e816b34b61
py-bay <github.com.tribunal325@passmail.net>
2025-09-02 14:56:49 +0200

    switch to mem for h2 and introduce minio configuration

--- src/main/java/com/ase/exagrad/studentservice/config/MinioProperties.java
+++ src/main/java/com/ase/exagrad/studentservice/config/MinioProperties.java
@@ -11,7 +11,7 @@ import java.util.Map;
 @Component
 @ConfigurationProperties(prefix = "minio")
 public class MinioProperties {
-    private String url;
+    private String endpoint;
     private String accessKey;
     private String secretKey;
     private Map<String, String> buckets;
