bed2685420a568271f02bf4fede92ceda0832631
Simon Dietrich <Simon.Dietrich@telekom.de>
2025-09-10 16:40:37 +0200

    Fix UnsatisfiedDependencyException: register FileProperties with @EnableConfigurationProperties

--- src/main/java/com/ase/exagrad/studentservice/config/FileProperties.java
+++ src/main/java/com/ase/exagrad/studentservice/config/FileProperties.java
@@ -4,9 +4,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 import lombok.Data;
 
-@ConfigurationProperties(prefix = "app.file")
 @Component
 @Data
+@ConfigurationProperties(prefix = "app.file")
 public class FileProperties {
   private long maxSize; // in bytes
   private String allowedTypes;
