6e9ab67fe53c09c402b1dd9bde5b7d98c45f5c64
py-bay <github.com.tribunal325@passmail.net>
2025-10-06 11:26:46 +0200

    fix tests

--- src/main/java/com/ase/exagrad/studentservice/config/SecurityConfig.java
+++ src/main/java/com/ase/exagrad/studentservice/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -18,9 +19,11 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
-        .csrf(AbstractHttpConfigurer::disable)
+        .csrf(csrf -> csrf
+            .ignoringRequestMatchers("/documents/**", "/actuator/**"))
         .authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
-        .sessionManagement(AbstractHttpConfigurer::disable);
+        .sessionManagement(session -> session
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
     return http.build();
   }
