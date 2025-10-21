493cc41091b5d21ab68cc4a4943d744f14117335
py-bay <github.com.tribunal325@passmail.net>
2025-10-21 15:38:48 +0200

    enhance CourseDataController with OpenAPI annotations for course retrieval

--- src/main/java/com/ase/exagrad/studentservice/controller/external/CourseDataController.java
+++ src/main/java/com/ase/exagrad/studentservice/controller/external/CourseDataController.java
@@ -9,18 +9,37 @@ import com.ase.exagrad.studentservice.component.ApiResponseFactory;
 import com.ase.exagrad.studentservice.dto.external.CourseDto;
 import com.ase.exagrad.studentservice.dto.response.ApiResponseWrapper;
 import com.ase.exagrad.studentservice.service.CourseService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/courses")
+@Tag(name = "Course Data", description = "Operations for retrieving course information")
 public class CourseDataController {
 
   private final CourseService courseService;
   private final ApiResponseFactory apiResponseFactory;
 
   @GetMapping
+  @Operation(
+      summary = "Get all courses",
+      description = "Retrieves a list of all available courses")
+  @ApiResponses(value = {
+      @ApiResponse(
+          responseCode = "200",
+          description = "Successfully retrieved course list",
+          content = @Content(schema = @Schema(implementation = ApiResponseWrapper.class))),
+      @ApiResponse(
+          responseCode = "500",
+          description = "Internal server error")
+  })
   public ResponseEntity<ApiResponseWrapper<List<CourseDto>>> getCourseData(
       HttpServletRequest request) {
     List<CourseDto> courses = courseService.getCourses();
