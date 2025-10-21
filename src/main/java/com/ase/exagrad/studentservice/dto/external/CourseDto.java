b600a0387d0ce2db9c4d29dcd0bff0863f92fcc8
py-bay <github.com.tribunal325@passmail.net>
2025-10-21 15:40:47 +0200

    reformat code

--- src/main/java/com/ase/exagrad/studentservice/dto/external/CourseDto.java
+++ src/main/java/com/ase/exagrad/studentservice/dto/external/CourseDto.java
@@ -4,11 +4,11 @@ import lombok.Data;
 
 @Data
 public class CourseDto {
-    private int semester;
-    private String exam_type;
-    private int credit_points;
-    private int total_units;
-    private int template_id;
-    private int id;
-    private TemplateDto template;
+  private int semester;
+  private String examType;
+  private int creditPoints;
+  private int totalUnits;
+  private int templateId;
+  private int id;
+  private TemplateDto template;
 }
