a5ea3da39d8a2bc89d429b22b09036a0a8fac4bf
Juna <annika.schatter@web.de>
2025-11-06 09:48:30 +0100

    fix url

--- src/main/java/com/ase/exagrad/studentservice/service/external/StudentService.java
+++ src/main/java/com/ase/exagrad/studentservice/service/external/StudentService.java
@@ -1,26 +1,26 @@
 package com.ase.exagrad.studentservice.service.external;
 
 import com.ase.exagrad.studentservice.dto.external.StudentDto;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
-@RequiredArgsConstructor
 public class StudentService {
 
-  private final WebClient.Builder webClientBuilder;
+  private final WebClient webClient;
+
+  public StudentService(WebClient.Builder webClientBuilder) {
+    this.webClient = webClientBuilder.baseUrl("https://sau-portal.de/team-11-api/api/v1/").build();
+  }
 
   @Value("${app.external-apis.student.base-url}")
   private String studentBaseUrl;
 
   public StudentDto fetchDataForStudent(String studentId) {
-    return webClientBuilder
-        .baseUrl(studentBaseUrl)
-        .build()
+    return webClient
         .get()
-        .uri("/users/{studentId}", studentId)
+        .uri("/users/{userId}", studentId)
         .retrieve()
         .bodyToMono(StudentDto.class)
         .block();
